51cf0b7d5f169e43181247dea7b3ace1bd8cfec7
borisenko <kk>
2020-12-06 11:51:34 +0300

    CSS

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="wrapper">
-    <div class="general-title">
+    <div class="topic">
       Вентилятор
     </div>
     <div class="power__title">
       Мощность
     </div>
-    <label>
-      <input v-model="formData.power" @input="submit" class="power__console">
+    <label class="power__console">
+      <input v-model="formData.power" @input="submit">
       кВт
     </label>
     <div class="director__title">
@@ -91,20 +91,137 @@ export default {
   }),
   methods: {
     submit() {
+      if(this.formData.power.length === 0) return  //предотвращаем отправку при пустом поле "Мощность".
       console.log('parameters =', this.formData)
-      let toSend = JSON.stringify(this.formData)   //toSend отсылаем на сервер.
+      let toSend = JSON.stringify(this.formData)   //отсылаем значение toSend на сервер.
     }
   }
 }
 </script>
 
 <style lang="scss">
+.general-title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  font-weight: bold;
+  color: chocolate;
+}
+
+.ff-row {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.ff-col {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.title {
+  font-weight: bold;
+  color: cadetblue;
+}
+
+label {
+  display: block;
+  margin-right: 5px;
+}
+
 .wrapper {
   max-width: 400px;
+  height: auto;
   margin: 60px auto;
-  border: cadetblue 1px solid;
+  border: #e6e8e8 1px dashed;
+  
+  font-size: 16px;
   
   display: grid;
+  grid-template-columns: 40% 60%;
+  grid-auto-rows: 60px;
   
+  @media (max-width: 400px) {  //адаптация на скрин, меньший 400px
+    width: 300px;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: space-between;
+  
+    & :nth-child(n) {
+      margin-bottom: 10px;
+    }
+  }
+  
+  .topic {
+    grid-area: 1 / 1 / 1 / span 2;
+    @extend .general-title;
+  }
+  
+  .options-title {
+    grid-area: 4 / 1 / 4 / span 2;
+    @extend .general-title;
+  }
+  
+  .power {
+    &__title {
+      grid-area: 2 / 1 / 2 / 1;
+      @extend .title;
+      @extend .ff-row;
+    }
+    
+    &__console {
+      grid-area: 2 / 2 / 2 / 2;
+      @extend .ff-row;
+      
+      input {
+        width: 40px;
+        height: 30px;
+        margin-right: 10px;
+        padding-left: 10px;
+      }
+    }
+  }
+  
+  .director {
+    &__title {
+      grid-area: 3 / 1 / 3 / 1;
+      @extend .title;
+      @extend .ff-row;
+    }
+    
+    &__console {
+      grid-area: 3 / 2 / 3 / 2;
+      @extend .ff-col;
+    }
+  }
+  
+  .shell {
+    &__title {
+      grid-area: 5 / 1 / 5 / 1;
+      @extend .title;
+      @extend .ff-row;
+    }
+    
+    &__console {
+      grid-area: 5 / 2 / 5 / 2;
+      @extend .ff-col;
+    }
+  }
+  
+  .additional {
+    &__title {
+      grid-area: 6 / 1 / 6 / 1;
+      @extend .title;
+      @extend .ff-row;
+      align-items: flex-start;
+    }
+    
+    &__console {
+      grid-area: 6 / 2 / 6 / 2;
+    }
+  }
 }
 </style>
